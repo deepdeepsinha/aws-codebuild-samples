2c8b9a4b4b81ad983b1ac98b07a31fef9beed17e
deepdeepsinha <50267657+deepdeepsinha@users.noreply.github.com>
2020-08-21 18:45:06 +0530

    ubuntu

--- deploy_scripts/start.sh
+++ deploy_scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
-cd /home/ec2-user/calculator
+source /var/www/.bash_profile
+cd /var/www/calculator
 NODE_ENV=production npm start > /dev/null 2> /dev/null < /dev/null &
